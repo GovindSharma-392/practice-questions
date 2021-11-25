a856f370ef0380aa843d1a88de44b3b2e0a59f1d
GovindSharma-392 <91676467+GovindSharma-392@users.noreply.github.com>
2021-11-25 17:54:52 +0530

    Add files via upload

---
+++ glq1.c.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,e;
+    printf("print all natural no. from 1 to :");
+    scanf("%d",&e);
+    i=1;
+    while(i<=e)
+    {
+        printf("%d,",i);
+    i++;
+    }
+
+    return 0;
+}
